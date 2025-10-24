f217605824852c8c852cf05c3cae5094243a1d2e
Andreas Vilhelmsson <addevilhelmsson@hotmail.com>
2025-10-24 22:43:26 +0200

    refactor: Simplify metadata and volumeMounts formatting in StatefulSet and remove unnecessary Kubernetes provider block

--- infra/terraform/providers-k8s.tf
+++ infra/terraform/providers-k8s.tf
@@ -8,9 +8,6 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.this.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.this.token
-  }
+  # Note: Ingress-nginx was installed manually via helm CLI due to timeout issues
+  # helm install ingress-nginx ingress-nginx/ingress-nginx -n ingress-nginx --create-namespace --wait=false
 }
